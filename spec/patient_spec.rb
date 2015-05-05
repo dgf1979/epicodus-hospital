d7a40e19d197e489e85321d0d2c9d3a726ec8a57
Drew & Kevin Chen <student@epicodus.com>
2015-05-05 14:17:33 -0700

    Write test for assigning doctor to patient

--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -11,4 +11,16 @@ describe(Patient) do
       expect(new_patient.birthdate()).to(eq('1979-09-30'))
     end
   end
+
+  describe('#assign_doctor') do
+    it ("assigns a doctor id to a patient") do
+      new_doctor = Doctor.new(:name => "Doctor", :specialty_id => 3, :id => nil)
+      new_doctor.save()
+      new_patient = Patient.new({ :name => 'Drew', :birthdate => '1979-09-30', :doctor_id => nil})
+      new_patient.save()
+      new_patient.assign_doctor(new_doctor.id())
+      expect(new_patient.doctor_id()).to(eq(new_doctor.id()))
+    end
+  end
+
 end

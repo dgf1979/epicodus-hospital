7f2444528ea94b04539d900fd721d56c5eb71904
Drew & Kevin Chen <student@epicodus.com>
2015-05-05 12:07:13 -0700

    add Doctor #init and #save to add and return a doctor

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+  attr_reader(:id, :name, :specialty_id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @specialty_id = attributes.fetch(:specialty_id)
+  end
+
+  define_method(:save) do
+    sql = "INSERT INTO doctor (name, specialty_id)
+    VALUES ('#{@name}',#{@specialty_id}) RETURNING id"
+    result = DB.exec(sql)
+    @id = result.first().fetch("id").to_i()
+  end
+
+
+end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -3,10 +3,11 @@ require('spec_helper')
 describe(Doctor) do
   describe('#initialize') do
     it("returns the id, name, and specialty of the doctor") do
-      new_doctor = Doctor.new({:name => 'Doogy Houser', :specialty => 1, :id => nil})
+      new_doctor = Doctor.new({:name => 'Doogy Houser', :specialty_id => 1, :id => nil})
+      new_doctor.save()
       expect(new_doctor.name()).to(eq('Doogy Houser'))
       expect(new_doctor.id()).to(be_an_instance_of(Fixnum))
-      expect(new_doctor.specialty()).to(eq(1))
+      expect(new_doctor.specialty_id()).to(eq(1))
     end
   end
 end

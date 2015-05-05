4be00d35bc10b6560f2cbe2546ab154543bdefcf
Drew & Kevin Chen <student@epicodus.com>
2015-05-05 13:33:37 -0700

    add database population and setup script

---
+++ setup/create_db.sql
@@ -0,0 +1,27 @@
+\c Guest
+
+DROP DATABASE hospital;
+DROP DATABASE hospital_test;
+
+CREATE DATABASE hospital;
+
+\c hospital
+
+CREATE TABLE doctor (id serial PRIMARY KEY, name varchar, specialty_id int);
+
+CREATE TABLE patient (id serial PRIMARY KEY, name varchar);
+
+CREATE TABLE specialty (id serial PRIMARY KEY, description varchar);
+
+INSERT INTO specialty (description) VALUES ('Family Medicine');
+INSERT INTO specialty (description) VALUES ('Diagnostic');
+INSERT INTO specialty (description) VALUES ('Oncology');
+INSERT INTO specialty (description) VALUES ('Neurology');
+INSERT INTO specialty (description) VALUES ('Surgery');
+INSERT INTO specialty (description) VALUES ('Psychiatry');
+
+CREATE DATABASE hospital_test WITH TEMPLATE hospital;
+
+\c hospital_test
+
+SELECT * FROM specialty

f934624c84031a5ecab96041f1316dae6d14f4b5
Drew & Kevin Chen <student@epicodus.com>
2015-05-05 14:09:25 -0700

    Make initialize and save method for patient

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,11 +2,15 @@ class Patient
   attr_reader(:id, :name, :birthdate, :doctor_id)
 
   define_method(:initialize) do |attributes|
-
+    @name = attributes.fetch(:name)
+    @birthdate = attributes.fetch(:birthdate)
+    @doctor_id = attributes.fetch(:doctor_id)
   end
 
   define_method(:save) do
-    []
+    sql = "INSERT INTO patient (name, birthdate) VALUES ('#{@name}', '#{@birthdate}') RETURNING id"
+    result = DB.exec(sql)
+    @id = result.first().fetch("id").to_i()
   end
 
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -4,7 +4,7 @@ describe(Patient) do
   #As an administrator, I want to add a patient with their name and birthdate.
   describe('#id') do
     it('adds a patient') do
-      new_patient = Patient.new({ :name => 'Drew', :birthdate => '1979-09-30'})
+      new_patient = Patient.new({ :name => 'Drew', :birthdate => '1979-09-30', :doctor_id => nil})
       new_patient.save()
       expect(new_patient.id()).to(be_an_instance_of(Fixnum))
       expect(new_patient.name()).to(eq('Drew'))
